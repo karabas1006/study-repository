cbb6093b1aab1597396e3ec165774067907f9d42
Karabas <ruslanrazzakow1006@gmail.com>
2026-02-26 13:02:34 +0500

    simple sql

--- main.go
+++ main.go
@@ -1,16 +1,44 @@
 package main
 
 import (
+	"context"
 	"fmt"
-
-	"study/feature1"
-	"study/feature2"
 	"study/feature_postgres/simple_connection"
+	"study/feature_postgres/simple_sql"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello, Git!")
-	feature1.Feature1()
-	feature2.Feature2()
-	simple_connection.CheckConnection()
+	ctx := context.Background()
+	conn, err := simple_connection.CreateConnection(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := simple_sql.CreateTable(ctx, conn); err != nil {
+		panic(err)
+	}
+
+	tasks, err := simple_sql.SelectRows(ctx, conn)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, task := range tasks {
+		if task.ID == 5 {
+			task.Title = "Pokormit koshku"
+			task.Description = "Otsypat koshke 30 gramm korma"
+			task.Completed = true
+			now := time.Now()
+			task.CompletedAt = &now
+
+			if err := simple_sql.UpdateTask(ctx, conn, task); err != nil {
+				panic(err)
+			}
+
+			break
+		}
+	}
+
+	fmt.Println("succeed!")
 }
