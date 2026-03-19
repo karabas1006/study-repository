5673dc60609e48ad9e890e26d07d36cd228c06cd
karabas <ruslanrazzakow1006@gmail.com>
2026-03-19 16:08:09 +0500

    add make file and env vars

--- feature_postgres/simple_connection/simple_connection.go
+++ feature_postgres/simple_connection/simple_connection.go
@@ -2,11 +2,12 @@ package simple_connection
 
 import (
 	"context"
+	"os"
 
 	"github.com/jackc/pgx/v5"
 )
 
 func CreateConnection(ctx context.Context) (*pgx.Conn, error) {
-
-	return pgx.Connect(ctx, "postgres://postgres:1006@localhost:5432/postgres")
+	connString := os.Getenv("CONN_STRING")
+	return pgx.Connect(ctx, connString)
 }
---
+++ migrations/000003_unique_phone.up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE users ADD CONSTRAINT user_phone_number_unique UNIQUE(phone_number);
